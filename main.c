07c4f6563737115a546ab96f6be55bed92f84cf0
contrbas42 <contrbas@gmail.com>
2018-01-07 00:46:51 +0200

    nothinf special

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int		main()
 {
+	//int argc && argv
 	char test[] = "[\n***** Start->0printf(, sdfsdfsdf);12printf(, sdfsdfsdf);34printf(, sdfsdfsdf);678printf(, sdfsdfsdf);9<-End *****\n]"; //should be the read from the file
 	char *result = result_without_printf(test);
 
